d46809b4c7bd7fce906c33176781614554bafffb
ErazerBrecht <brecht.carlier@student.ap.be>
2015-08-18 22:00:05 +0200

    Added BerekenModule - Implemented first GUI

---
+++ BerekenModule.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ISIS
+{
+    /// <summary>
+    /// Interaction logic for BerekenModule.xaml
+    /// </summary>
+    public partial class BerekenModule : UserControl
+    {
+        ISIS_DataEntities _entities;
+        int _oldLengthSearchBox = 0;
+
+        public BerekenModule()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int tempID;
+            var tempSearchList = new List<Klanten>();
+
+            //If text is getting longer search in items from the combobox self
+            //If not search in the list with all the items (in this situation you're deleting chars)
+            //Result: If you're adding chars, it will go faster because you need to search in a smaller list!
+            if (_oldLengthSearchBox < TextBoxSearch.Text.Length)
+            {
+                tempSearchList = TextBoxSearch.Items.OfType<Klanten>().ToList();
+            }
+            else
+            {
+                tempSearchList = _entities.Klanten.Local.ToList();
+            }
+
+            if (int.TryParse(TextBoxSearch.Text, out tempID))
+            {
+                List<Klanten> tempList = tempSearchList.Where(k => k.ID.ToString().Contains(TextBoxSearch.Text)).ToList();
+                TextBoxSearch.ItemsSource = tempList;
+            }
+            else
+            {
+                //If the text contains off digits don't search after it in Naam and Voornaam (after all you will not find anything!)
+                if (!TextBoxSearch.Text.Any(char.IsDigit))
+                {
+                    List<Klanten> tempList = tempSearchList.Where(k => k.Naam.ToString().ToLower().Contains(TextBoxSearch.Text.ToLower()) || k.Voornaam != null && k.Voornaam.ToString().ToLower().Contains(TextBoxSearch.Text.ToLower())).ToList();
+                    TextBoxSearch.ItemsSource = tempList;
+                }
+            }
+
+            _oldLengthSearchBox = TextBoxSearch.Text.Length;        //Save current textlength, in the future we can compare it to determine if the textlength became bigger or smaller!
+        }
+
+        private void TextBoxSearch_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (TextBoxSearch.SelectedItem is Klanten)
+            {
+                Klanten temp = (Klanten)TextBoxSearch.SelectedItem;
+                //TODO: ADD CODE TO USE CORRECT KLANT!!!
+            }
+        }
+    }
+
+    public class BorderGrid : Grid
+    {
+        protected override void OnRender(DrawingContext dc)
+        {
+            double leftOffset = 0;
+            double topOffset = 0;
+            Pen pen = new Pen(Brushes.Black, 3);
+            pen.Freeze();
+
+            foreach (RowDefinition row in this.RowDefinitions)
+            {
+                dc.DrawLine(pen, new Point(0, topOffset), new Point(this.ActualWidth, topOffset));
+                topOffset += row.ActualHeight;
+            }
+            // draw last line at the bottom
+            dc.DrawLine(pen, new Point(0, topOffset), new Point(this.ActualWidth, topOffset));
+
+            foreach (ColumnDefinition column in this.ColumnDefinitions)
+            {
+                dc.DrawLine(pen, new Point(leftOffset, 0), new Point(leftOffset, this.ActualHeight));
+                leftOffset += column.ActualWidth;
+            }
+            //draw last line on the right
+            dc.DrawLine(pen, new Point(leftOffset, 0), new Point(leftOffset, this.ActualHeight));
+
+            base.OnRender(dc);
+        }
+    }
+}
