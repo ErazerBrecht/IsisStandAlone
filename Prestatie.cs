9d38cca66109726b76ca7b9d5d84f41e66452b31
ErazerBrecht <brecht.carlier@student.ap.be>
2015-08-19 13:32:46 +0200

    Updated DB --> Changed to NOT NULL for Prestatie Fields            --> Changed to NOT NULL for Tegoed (Klanten) Updated EF Model and etc.

--- Prestatie.cs
+++ Prestatie.cs
@@ -27,24 +27,26 @@ namespace ISIS
         public System.DateTime Datum { get { return _datum; } set { _datum = value; OnPropertyChanged("Datum");} }
     private int _klantennummer;
         public int KlantenNummer { get { return _klantennummer; } set { _klantennummer = value; OnPropertyChanged("KlantenNummer");} }
-    private Nullable<byte> _aantalhemden;
-        public Nullable<byte> AantalHemden { get { return _aantalhemden; } set { _aantalhemden = value; OnPropertyChanged("AantalHemden");} }
-    private Nullable<byte> _parameterhemden;
-        public Nullable<byte> ParameterHemden { get { return _parameterhemden; } set { _parameterhemden = value; OnPropertyChanged("ParameterHemden");} }
-    private Nullable<byte> _aantallakens1;
-        public Nullable<byte> AantalLakens1 { get { return _aantallakens1; } set { _aantallakens1 = value; OnPropertyChanged("AantalLakens1");} }
-    private Nullable<byte> _parameterlakens1;
-        public Nullable<byte> ParameterLakens1 { get { return _parameterlakens1; } set { _parameterlakens1 = value; OnPropertyChanged("ParameterLakens1");} }
-    private Nullable<byte> _aantallakens2;
-        public Nullable<byte> AantalLakens2 { get { return _aantallakens2; } set { _aantallakens2 = value; OnPropertyChanged("AantalLakens2");} }
-    private Nullable<byte> _parameterlakens2;
-        public Nullable<byte> ParameterLakens2 { get { return _parameterlakens2; } set { _parameterlakens2 = value; OnPropertyChanged("ParameterLakens2");} }
-    private Nullable<byte> _aantalanderestrijk;
-        public Nullable<byte> AantalAndereStrijk { get { return _aantalanderestrijk; } set { _aantalanderestrijk = value; OnPropertyChanged("AantalAndereStrijk");} }
-    private Nullable<byte> _tijdanderestrijk;
-        public Nullable<byte> TijdAndereStrijk { get { return _tijdanderestrijk; } set { _tijdanderestrijk = value; OnPropertyChanged("TijdAndereStrijk");} }
-    private Nullable<byte> _parameteradministratie;
-        public Nullable<byte> ParameterAdministratie { get { return _parameteradministratie; } set { _parameteradministratie = value; OnPropertyChanged("ParameterAdministratie");} }
+    private byte _aantalhemden;
+        public byte AantalHemden { get { return _aantalhemden; } set { _aantalhemden = value; OnPropertyChanged("AantalHemden");} }
+    private byte _parameterhemden;
+        public byte ParameterHemden { get { return _parameterhemden; } set { _parameterhemden = value; OnPropertyChanged("ParameterHemden");} }
+    private byte _aantallakens1;
+        public byte AantalLakens1 { get { return _aantallakens1; } set { _aantallakens1 = value; OnPropertyChanged("AantalLakens1");} }
+    private byte _parameterlakens1;
+        public byte ParameterLakens1 { get { return _parameterlakens1; } set { _parameterlakens1 = value; OnPropertyChanged("ParameterLakens1");} }
+    private byte _aantallakens2;
+        public byte AantalLakens2 { get { return _aantallakens2; } set { _aantallakens2 = value; OnPropertyChanged("AantalLakens2");} }
+    private byte _parameterlakens2;
+        public byte ParameterLakens2 { get { return _parameterlakens2; } set { _parameterlakens2 = value; OnPropertyChanged("ParameterLakens2");} }
+    private byte _aantalanderestrijk;
+        public byte AantalAndereStrijk { get { return _aantalanderestrijk; } set { _aantalanderestrijk = value; OnPropertyChanged("AantalAndereStrijk");} }
+    private byte _tijdanderestrijk;
+        public byte TijdAndereStrijk { get { return _tijdanderestrijk; } set { _tijdanderestrijk = value; OnPropertyChanged("TijdAndereStrijk");} }
+    private byte _parameteranderestrijk;
+        public byte ParameterAndereStrijk { get { return _parameteranderestrijk; } set { _parameteranderestrijk = value; OnPropertyChanged("ParameterAndereStrijk");} }
+    private byte _tijdadministratie;
+        public byte TijdAdministratie { get { return _tijdadministratie; } set { _tijdadministratie = value; OnPropertyChanged("TijdAdministratie");} }
     
         public virtual Klant Klanten { get; set; }
     }
