319d1ff469a828a4352fd814c74ac7222684edc2
ErazerBrecht <brecht.carlier@student.ap.be>
2015-08-21 19:47:56 +0200

    Added INotifyPropertyChanged in Parameter class! (In the Uitbreiding file)

--- Parameters.cs
+++ Parameters.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace ISIS
 {
-    class Parameters
+    class Parameters : INotifyPropertyChanged
     {
         public Parameters()
         {
@@ -14,11 +15,59 @@ namespace ISIS
             ParameterLakens1 = Properties.Settings.Default.ParameterLakens1;
             ParameterLakens2 = Properties.Settings.Default.ParameterLakens2;
             ParameterAndereStrijk = Properties.Settings.Default.ParameterAndereStrijk;
-        } 
+        }
 
-        public byte ParameterHemden { get; set; }
-        public byte ParameterLakens1 { get; set; }
-        public byte ParameterLakens2 { get; set; }
-        public byte ParameterAndereStrijk { get; set; }
+        private byte _parameterHemden;
+        public byte ParameterHemden
+        {
+            get { return _parameterHemden; }
+            set
+            {
+                _parameterHemden = value;
+                NoticeMe("ParameterHemden");
+            }
+        }
+
+        private byte _parameterLakens1;
+        public byte ParameterLakens1
+        {
+            get { return _parameterLakens1; }
+            set
+            {
+                _parameterLakens1 = value;
+                NoticeMe("ParameterLakens1");
+            }
+        }
+
+        private byte _parameterLakens2;
+        public byte ParameterLakens2
+        {
+            get { return _parameterLakens2; }
+            set
+            {
+                _parameterLakens2 = value;
+                NoticeMe("ParameterLakens2");
+            }
+        }
+
+        private byte _parameterAndereStrijk;
+        public byte ParameterAndereStrijk
+        {
+            get { return _parameterAndereStrijk; }
+            set
+            {
+                _parameterAndereStrijk = value;
+                NoticeMe("ParameterAndereStrijk");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NoticeMe(string property)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+        }
+
     }
 }
