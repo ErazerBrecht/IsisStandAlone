c8cf27eefde8c4c228de73486979780946f16005
ErazerBrecht <brecht.carlier@student.ap.be>
2015-09-14 20:32:06 +0200

    Add ParameterBeheer back => MVVM approach

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -28,6 +28,7 @@ namespace ISIS.ViewModels
                 TestConnection();
                 Workspaces = new ObservableCollection<WorkspaceViewModel>();
                 Workspaces.Add(new BerekenModuleViewModel());
+                Workspaces.Add(new PrestatieBeheerViewModel());
                 Workspaces.Add(new KlantenBeheerViewModel());
                 Workspaces.Add(new PersoneelBeheerViewModel());
                 Workspaces.Add(new ParameterBeheerViewModel());
