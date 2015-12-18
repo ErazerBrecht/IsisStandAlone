ee9f6ea51517824b9c9a0c36f22ec2b0ee8213ed
ErazerBrecht <brecht.carlier@student.ap.be>
2015-12-18 16:48:43 +0100

    Changed searchboxklanten to something else on PrestatieBeheer

--- ViewModels/PrestatieBeheerViewModel.cs
+++ ViewModels/PrestatieBeheerViewModel.cs
@@ -5,14 +5,14 @@ using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 using System.Windows.Data;
 
 namespace ISIS.ViewModels
 {
-    class PrestatieBeheerViewModel : WorkspaceViewModel, ISelectedKlant
+    class PrestatieBeheerViewModel : WorkspaceViewModel
     {
         ISIS_DataEntities ctx;
-        public SearchBoxKlantViewModel SearchBoxViewModel { get; set; }
 
         private List<TijdPrestatie> _originalPrestaties;
 
@@ -30,46 +30,90 @@ namespace ISIS.ViewModels
             }
         }
 
-        //#region FilterOnName
-        //private bool _enableSearch;
-        //public bool EnableSearch
-        //{
-        //    get
-        //    {
-        //        return _enableSearch;
-        //    }
-        //    private set
-        //    {
-        //        _enableSearch = value;
-        //        NoticeMe("EnableSearch");
-        //    }
-        //}
+        #region Klanten Searchbox
+        #region SelectedKlant full property
+        private Klant _selectedKlant;
+        public Klant SelectedKlant
+        {
+            get
+            {
+                return _selectedKlant;
+            }
 
-        //private bool _boolIedereen;
-        //public bool BoolIedereen
-        //{
-        //    get
-        //    {
-        //        return _boolIedereen;
-        //    }
-        //    set
-        //    {
-        //        _boolIedereen = value;
-        //        NoticeMe("BoolIedereen");
+            set
+            {
+                _selectedKlant = value;
+                Filter();
+            }
+        }
+        #endregion
 
-        //        if (value == true)
-        //        {
-        //            EnableSearch = false;
-        //        }
-        //        else
-        //        {
-        //            EnableSearch = true;
-        //        }
+        #region Klanten list for searchbox full property
+        private IEnumerable<Klant> _klanten;
+        public IEnumerable<Klant> Klanten
+        {
+            get { return _klanten; }
+            set
+            {
+                _klanten = value;
+                NoticeMe("Strijkers");
+            }
+        }
+        #endregion
+        public AutoCompleteFilterPredicate<object> KlantenFilter
+        {
+            get
+            {
+                return (searchText, obj) =>
+                {
+                    var klant = obj as Klant;
+                    return klant != null && (Convert.ToString(klant.ID).Contains(searchText) || klant.Naam.ToUpper().Contains(searchText.ToUpper()) || (!string.IsNullOrEmpty(klant.Voornaam) && klant.Voornaam.ToUpper().Contains(searchText.ToUpper())));
+                };
+            }
+        }
 
-        //        Filter();
-        //    }
-        //}
-        //#endregion
+        #endregion
+
+        #region FilterOnName
+        private bool _enableSearch;
+        public bool EnableSearch
+        {
+            get
+            {
+                return _enableSearch;
+            }
+            private set
+            {
+                _enableSearch = value;
+                NoticeMe("EnableSearch");
+            }
+        }
+
+        private bool _boolIedereen;
+        public bool BoolIedereen
+        {
+            get
+            {
+                return _boolIedereen;
+            }
+            set
+            {
+                _boolIedereen = value;
+                NoticeMe("BoolIedereen");
+
+                if (value == true)
+                {
+                    EnableSearch = false;
+                }
+                else
+                {
+                    EnableSearch = true;
+                }
+
+                Filter();
+            }
+        }
+        #endregion
 
         //#region FilterOnDate
 
@@ -142,27 +186,11 @@ namespace ISIS.ViewModels
 
         //#endregion
 
-        private Klant _selectedKlant;
-        public Klant SelectedKlant
-        {
-            get
-            {
-                return _selectedKlant;
-            }
-
-            set
-            {
-                _selectedKlant = value;
-                Filter();
-            }
-        }
-
         public PrestatieBeheerViewModel()
         {
             Header = "PrestatieBeheer";
             LoadData();
-            //BoolIedereen = true;
-            SearchBoxViewModel = new SearchBoxKlantViewModel(this);
+            BoolIedereen = true;
             //BoolDag = true;
         }
 
@@ -171,17 +199,18 @@ namespace ISIS.ViewModels
             ctx = new ISIS_DataEntities();
             _originalPrestaties = ctx.TijdPrestaties.ToList();
             Prestaties = _originalPrestaties;
+            Klanten = ctx.Klanten.ToList();
         }
 
         public void Filter()
         {
-            //if (BoolIedereen)
-            //    Prestaties = _originalPrestaties;
-            //else
-            //{
-            //    if (SelectedKlant != null)
-            //        Prestaties = _originalPrestaties.Where(p => p.KlantenNummer == _selectedKlant.ID).ToList();
-            //}
+            if (BoolIedereen)
+                Prestaties = _originalPrestaties;
+            else
+            {
+                if (SelectedKlant != null)
+                    Prestaties = _originalPrestaties.Where(p => p.Prestaties.KlantenNummer == _selectedKlant.ID).ToList();
+            }
 
             //if(FilterOpDag)
             //{
